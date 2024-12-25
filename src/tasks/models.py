5a5c34896eeb9dd445ea2353ab40724fe4a5820e
HardCodeMatter <dklymovets1@gmail.com>
2024-12-25 23:55:14 +0200

    Added priority field to Task model. Created priority in Task schema with validator

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,10 +1,17 @@
 import uuid
+from enum import Enum
 from datetime import datetime
 
-from sqlalchemy import func
+from sqlalchemy import func, Enum as SQLAlchemyEnum
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
+
+
+class Priority(str, Enum):
+    low = 'low'
+    medium = 'medium'
+    high = 'high'
 
 
 class Task(Base):
@@ -15,12 +22,13 @@ class Task(Base):
     description: Mapped[str] = mapped_column(nullable=True)
 
     is_completed: Mapped[bool] = mapped_column(default=False)
+    priority: Mapped[str] = mapped_column(SQLAlchemyEnum(Priority), server_default=Priority.low, nullable=False)
 
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
 
     def __str__(self) -> str:
-        return f'Task(id="{self.id}", title="{self.title}", is_completed="{self.is_completed}")'
+        return f'Task(id="{self.id}", title="{self.title}", is_completed="{self.is_completed}", priority="{self.priority}")'
 
     def __repr__(self) -> str:
         return self.__str__()
--- src/tasks/schemas.py
+++ src/tasks/schemas.py
@@ -3,10 +3,13 @@ from datetime import datetime
 from fastapi import HTTPException, status
 from pydantic import BaseModel, field_validator
 
+from .models import Priority
+
 
 class TaskBase(BaseModel):
     title: str | None = None
     description: str | None = None
+    priority: Priority
 
     @field_validator('title', mode='before')
     @classmethod
@@ -24,6 +27,17 @@ class TaskBase(BaseModel):
 
         return cls.validate_length(value, 'Description', 0, 200)
     
+    @field_validator('priority', mode='before')
+    @classmethod
+    def validate_priority(cls, value: str) -> str:
+        if value not in Priority.__members__.values():
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f'Unknown value \'{value}\' for priority.',
+            )
+        
+        return value
+    
     @staticmethod
     def validate_length(value: str, field_name: str, min_length: int, max_length: int) -> str:
         if len(value.strip()) < min_length or len(value.strip()) > max_length:
@@ -40,6 +54,7 @@ class TaskRead(BaseModel):
     title: str
     description: str
     is_completed: bool
+    priority: Priority
     created_at: datetime
     updated_at: datetime
 
