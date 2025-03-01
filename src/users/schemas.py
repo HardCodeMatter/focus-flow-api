a4643af14a2c285abbc937a8cdaf886009153826
HardCodeMatter <dklymovets1@gmail.com>
2025-03-01 11:33:07 +0200

    Created email and password validators. Added None value checking for required fields. Updated exception detail for username and fullname fields

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -29,7 +29,7 @@ class UserBase(BaseModel):
         if not re.match(r'^[A-Za-z]+(\.?)(( [A-Za-z]+(\.?)?)+)?$', value):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Fullname is not matching the pattern.'
+                detail='Fullname must contains only letters, spaces and optional periods.'
             )
 
         return cls.validate_length(value, 'Fullname', 0, 64)
@@ -38,15 +38,35 @@ class UserBase(BaseModel):
     @classmethod
     def validate_username(cls, value: str) -> str:
         if value is None:
-            return value
-        
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Username is required.',
+            )
+
         if not re.match(r'^(?!.*\.\.)([A-Za-z0-9_][A-Za-z0-9._]{1,28}[A-Za-z0-9_])$', value):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail='Username is not matching the pattern.'
+                detail='Username must contains at least one letter and one number.'
             )
 
         return cls.validate_length(value, 'Username', 3, 30)
+    
+    @field_validator('email')
+    @classmethod
+    def validate_email(cls, value: str) -> str:
+        if value is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Email is required.',
+            )
+        
+        if not re.match(r'^[\w\.\+\-]+\@[\w]+\.[a-z]{2,}(\.[a-z]{2,})?$', value):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Email is not valid.',
+            )
+
+        return cls.validate_length(value, 'Email', 10, 128)
 
     @staticmethod
     def validate_length(value: str, field_name: str, min_length: int, max_length: int) -> str:
@@ -73,6 +93,23 @@ class UserRead(BaseModel):
 
 class UserCreate(UserBase):
     hashed_password: str
+
+    @field_validator('hashed_password')
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        if value is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Password is required.',
+            )
+
+        if not re.match(r'^(?=.*[0-9])(?=.*[a-zA-Z])', value):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Password must contain at least one letter and one number.',
+            )
+
+        return cls.validate_length(value, 'Password', 6, 32)
 
 
 class UserUpdate(UserBase): ...
